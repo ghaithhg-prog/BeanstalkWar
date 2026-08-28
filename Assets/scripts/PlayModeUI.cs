6cc02c1455bbaa4204a4e5be35e2838f9dc8c7e0
Mohamed Ghaith Guirat <ghaithhg@gmail.com>
2026-08-28 22:48:28 +0100

    Add networked team and character selection flow

---
+++ Assets/scripts/PlayModeUI.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayModeUI : MonoBehaviour
+{
+    public enum PlayMode
+    {
+        None,
+        Online,
+        Offline
+    }
+
+    public static PlayMode SelectedMode =
+        PlayMode.None;
+
+    public GameObject panel;
+    public Button onlineButton;
+    public Button offlineButton;
+
+    void Awake()
+    {
+        onlineButton.onClick.AddListener(
+            SelectOnline
+        );
+
+        offlineButton.onClick.AddListener(
+            SelectOffline
+        );
+    }
+
+    void SelectOnline()
+    {
+        SelectedMode = PlayMode.Online;
+
+        Debug.Log("ONLINE mode selected");
+
+        panel.SetActive(false);
+    }
+
+    void SelectOffline()
+    {
+        SelectedMode = PlayMode.Offline;
+
+        Debug.Log("OFFLINE mode selected");
+
+        panel.SetActive(false);
+    }
+}
